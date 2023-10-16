ac230b0fa5b8b18bfe84a9b67c9116787ab95cde
iury.magano <iury.magano@beesix.com.br>
2023-10-15 21:24:12 -0300

    add fonts

--- App.tsx
+++ App.tsx
@@ -1,8 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
 import Home from "./src/pages/Home/Home";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "Inter-Regular": require("./assets/fonts/Inter-Regular.ttf"),
+  });
+
   return (
     <View>
       <StatusBar backgroundColor="#1d1d2e" style="light" translucent={false} />
